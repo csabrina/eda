164671132196df7b744790ce2fb82114de7a3f96
sabrina.costa <sabrina.costa.ext@synchro.com.br>
2024-03-20 09:37:30 -0300

    cra classe lista duplamente encadeada

--- estruturasLineanes/listaDEncadeada/ListaDEncadeada.java
+++ estruturasLineanes/listaDEncadeada/ListaDEncadeada.java
@@ -1,7 +1,15 @@
 package estruturasLineanes.listaDEncadeada;
 
 public class ListaDEncadeada {
-    public Node last;
-    public Node first;
+    private Node head; //referência para o primeiro nodo da lista
+    private Node tail; // referência para o último nodo da lista
+    private int count; // contador para o número de elementos na lista
+
+    // construtor para inicializar a lista vazia
+    public ListaDEncadeada(int count, Node head, Node tail) {
+        this.head = null; // inicialmente, a lista está vazia, então a cabeça é nula
+        this.tail = null; // inicialmente, a lista está vazia, então a cauda é nula
+        this.count = 0; // inicialmente, a lista não tem elementos
+    }
 
 }
